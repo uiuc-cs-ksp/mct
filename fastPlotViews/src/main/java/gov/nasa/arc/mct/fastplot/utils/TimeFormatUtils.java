98ba0e68473743b825d2ebc69146dfc1bdce9322
Dan Berrios <daniel.c.berrios@nasa.gov>
2012-07-09 17:07:42 -0700

    Add new files

---
+++ fastPlotViews/src/main/java/gov/nasa/arc/mct/fastplot/utils/TimeFormatUtils.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Mission Control Technologies, Copyright (c) 2009-2012, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space 
+ * Administration. All rights reserved.
+ *
+ * The MCT platform is licensed under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ *
+ * MCT includes source code licensed under additional open source licenses. See 
+ * the MCT Open Source Licenses file included with this distribution or the About 
+ * MCT Licenses dialog available at runtime from the MCT Help menu for additional 
+ * information. 
+ *******************************************************************************/
+package gov.nasa.arc.mct.fastplot.utils;
+
+import gov.nasa.arc.mct.fastplot.bridge.PlotConstants;
+import gov.nasa.arc.mct.services.activity.TimeService;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimeFormatUtils {
+	private final static Logger logger = LoggerFactory.getLogger(TimeFormatUtils.class);
+
+	private static final String ignoreNanoSeconds = "eeee";
+	
+	public static SimpleDateFormat makeDataFormat(String formatString) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(TimeService.DEFAULT_TIME_FORMAT);
+		dateFormat.setTimeZone(TimeZone.getTimeZone(PlotConstants.DEFAULT_TIME_ZONE));
+		
+		if (formatString != null && !formatString.isEmpty()) {
+			try {
+				if (formatString.endsWith(ignoreNanoSeconds)) {
+					formatString = formatString.substring(0, formatString.indexOf(ignoreNanoSeconds));
+				}
+
+				dateFormat = new SimpleDateFormat(formatString);
+			} catch (IllegalArgumentException e) { 
+				logger.error("Unable to format date time format: "+ formatString+ ". Instead using default: "+ TimeService.DEFAULT_TIME_FORMAT);
+			}
+		}
+		return dateFormat;
+
+	}
+}
---
+++ platform/src/main/java/gov/nasa/arc/mct/gui/actions/ResetFeedViewsAction.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Mission Control Technologies, Copyright (c) 2009-2012, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space 
+ * Administration. All rights reserved.
+ *
+ * The MCT platform is licensed under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ *
+ * MCT includes source code licensed under additional open source licenses. See 
+ * the MCT Open Source Licenses file included with this distribution or the About 
+ * MCT Licenses dialog available at runtime from the MCT Help menu for additional 
+ * information. 
+ *******************************************************************************/
+package gov.nasa.arc.mct.gui.actions;
+
+import gov.nasa.arc.mct.api.feed.FeedDataArchive;
+import gov.nasa.arc.mct.gui.ActionContext;
+import gov.nasa.arc.mct.gui.ActionContextImpl;
+import gov.nasa.arc.mct.gui.ContextAwareAction;
+import gov.nasa.arc.mct.platform.PlatformImpl;
+import gov.nasa.arc.mct.platform.spi.SubscriptionManager;
+import gov.nasa.arc.mct.services.component.FeedManager;
+
+import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResetFeedViewsAction extends ContextAwareAction {
+    private static final long serialVersionUID = 217492157805448386L;
+    
+    private static final Logger logger = LoggerFactory.getLogger(ResetFeedViewsAction.class);
+    
+    private ActionContextImpl actionContext;
+    private static final ResourceBundle BUNDLE = 
+        ResourceBundle.getBundle("ResetBufferMenu");
+    
+    private static String TEXT = BUNDLE.getString("ResetFeedMenu");
+    
+    public ResetFeedViewsAction() {
+        super(TEXT);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        JFrame parentFrame = actionContext.getTargetHousing().getHostedFrame();
+        int confirmationResult = JOptionPane.showConfirmDialog(parentFrame,
+                String.format(BUNDLE.getString("ResetBufferConfirmDialogMsg")),
+                BUNDLE.getString("ResetBufferConfirmDialogTitle"),
+                JOptionPane.YES_NO_OPTION);
+                
+        if (confirmationResult == 0) {
+            FeedDataArchive dataArchive =PlatformImpl.getInstance().getFeedDataArchive();            
+            if (dataArchive != null) {
+                dataArchive.reset();
+            } else {
+                logger.error("DataArchive is null.");
+            }
+             
+            FeedManager feedManager = PlatformImpl.getInstance().getFeedManager();
+            if (feedManager != null) {
+        	    feedManager.clear();
+            } else {
+                logger.error("FeedManager is null.");
+            }
+             
+            SubscriptionManager mgr = PlatformImpl.getInstance().getSubscriptionManager();
+            if (mgr != null) {
+                mgr.refresh();
+            } else {
+                logger.error("SubscriptionManager is null.");
+            }
+        } 
+    }
+
+    @Override
+    public boolean canHandle(ActionContext context) {
+        actionContext = (ActionContextImpl) context;
+        if (actionContext.getTargetHousing() == null) {
+            return false;
+        }
+        return true;
+    }
+}
