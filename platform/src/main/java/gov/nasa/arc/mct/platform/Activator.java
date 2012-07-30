22421a8f90a61818b516ff5da891277988b19c2d
Christopher Webster <chris.webster@nasa.gov>
2012-07-30 11:06:42 -0700

    Make platform, util, and mctcore OSGI bundles

---
+++ platform/src/main/java/gov/nasa/arc/mct/platform/Activator.java
@@ -0,0 +1,83 @@
+package gov.nasa.arc.mct.platform;
+
+import gov.nasa.arc.mct.context.GlobalContext;
+import gov.nasa.arc.mct.defaults.view.DefaultViewProvider;
+import gov.nasa.arc.mct.exception.DefaultExceptionHandler;
+import gov.nasa.arc.mct.gui.FeedManagerImpl;
+import gov.nasa.arc.mct.gui.impl.MenuExtensionManager;
+import gov.nasa.arc.mct.identitymgr.impl.IdentityManagerFactory;
+import gov.nasa.arc.mct.osgi.platform.EquinoxOSGIRuntimeImpl;
+import gov.nasa.arc.mct.platform.spi.PersistenceProvider;
+import gov.nasa.arc.mct.platform.spi.Platform;
+import gov.nasa.arc.mct.platform.spi.PlatformAccess;
+import gov.nasa.arc.mct.policymgr.PolicyManagerImpl;
+import gov.nasa.arc.mct.registry.ExternalComponentRegistryImpl;
+import gov.nasa.arc.mct.services.component.FeedManager;
+import gov.nasa.arc.mct.services.component.MenuManager;
+import gov.nasa.arc.mct.services.component.PolicyManager;
+import gov.nasa.arc.mct.util.exception.MCTException;
+import gov.nasa.arc.mct.util.logging.MCTLogger;
+
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    private static final MCTLogger logger = MCTLogger.getLogger(Activator.class);
+    
+    final Timer t = new Timer("MCT Launch check timer", true);    
+    
+    @Override
+    public void start(final BundleContext context) {
+        DefaultExceptionHandler defaultExceptionHandler = new DefaultExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(defaultExceptionHandler);
+       
+        initServicesAndHandlers(context);
+        
+        // wait one minute and then check to see if a PersistenceProvider has been installed
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (context.getServiceReference(PersistenceProvider.class.getName()) == null) {
+                    logger.error("unable to obtain persistence provider");
+                    System.exit(0);
+                }
+            }
+        }, 60000);
+    }
+    
+    private void initServicesAndHandlers(BundleContext bc) {
+        @SuppressWarnings("rawtypes")
+        Dictionary d = new Properties();
+        PlatformImpl.getInstance().setBundleContext(bc);
+        (new PlatformAccess()).setPlatform(PlatformImpl.getInstance());
+        try {
+            GlobalContext.getGlobalContext().setIdManager(IdentityManagerFactory.newIdentityManager(null));
+        } catch (MCTException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        EquinoxOSGIRuntimeImpl.getOSGIRuntime().setBundleContext(bc);
+        bc.registerService(new String[] { gov.nasa.arc.mct.services.component.ComponentRegistry.class
+                .getName() }, ExternalComponentRegistryImpl.getInstance(), d);
+
+        bc.registerService(new String[] { PolicyManager.class.getName() }, PolicyManagerImpl.getInstance(),d);
+        bc.registerService(new String[] { FeedManager.class.getName() }, FeedManagerImpl.getInstance(),d);
+        bc.registerService(new String[] { MenuManager.class.getName() }, MenuExtensionManager.getInstance(),d);
+        bc.registerService(new String[] { Platform.class.getName() }, PlatformImpl.getInstance(),d);
+
+        ExternalComponentRegistryImpl.getInstance().setDefaultViewProvider(new DefaultViewProvider());
+    }
+
+    @Override
+    public void stop(BundleContext arg0) throws Exception {
+    	
+    }
+
+}
---
+++ platform/src/main/java/gov/nasa/arc/mct/platform/LaunchMCTService.java
@@ -0,0 +1,217 @@
+package gov.nasa.arc.mct.platform;
+/*******************************************************************************
+ * Mission Control Technologies, Copyright (c) 2009-2012, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space 
+ * Administration. All rights reserved.
+ *
+ * The MCT platform is licensed under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ *
+ * MCT includes source code licensed under additional open source licenses. See 
+ * the MCT Open Source Licenses file included with this distribution or the About 
+ * MCT Licenses dialog available at runtime from the MCT Help menu for additional 
+ * information. 
+ *******************************************************************************/
+import gov.nasa.arc.mct.components.AbstractComponent;
+import gov.nasa.arc.mct.context.GlobalContext;
+import gov.nasa.arc.mct.gui.impl.MenuExtensionManager;
+import gov.nasa.arc.mct.gui.impl.StatusAreaWidgetRegistryImpl;
+import gov.nasa.arc.mct.osgi.platform.EquinoxOSGIRuntimeImpl;
+import gov.nasa.arc.mct.osgi.platform.OSGIRuntime;
+import gov.nasa.arc.mct.platform.spi.PersistenceProvider;
+import gov.nasa.arc.mct.platform.spi.Platform;
+import gov.nasa.arc.mct.platform.spi.PlatformAccess;
+import gov.nasa.arc.mct.policymgr.PolicyManagerImpl;
+import gov.nasa.arc.mct.registry.ExternalComponentRegistryImpl;
+import gov.nasa.arc.mct.registry.ExternalComponentRegistryImpl.ExtendedComponentProvider;
+import gov.nasa.arc.mct.search.SearchServiceImpl;
+import gov.nasa.arc.mct.services.component.ComponentProvider;
+import gov.nasa.arc.mct.services.internal.component.User;
+import gov.nasa.arc.mct.util.LookAndFeelSettings;
+import gov.nasa.arc.mct.util.exception.MCTRuntimeException;
+import gov.nasa.arc.mct.util.logging.MCTLogger;
+import gov.nasa.arc.mct.util.property.MCTProperties;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public class LaunchMCTService {
+    
+    private static final MCTLogger logger = MCTLogger.getLogger(LaunchMCTService.class);
+    private final Collection<ServiceReference> allProviders = new ArrayList<ServiceReference>();
+    private boolean componentProvidersChanged = false;
+    private boolean requiresRefresh = false;
+    private boolean pluginsLoaded = false;
+    private Timer t;
+            
+    public void bind(PersistenceProvider provider) {
+        // do nothing as this signals launch can proceed
+    }
+    
+    public void unbind(PersistenceProvider provider) {
+        
+    }
+    
+    private synchronized void notifyPlatformServicesChanged() {
+        List<ExtendedComponentProvider> providers = new ArrayList<ExtendedComponentProvider>(allProviders.size());
+        for (ServiceReference sr:allProviders) {
+            ComponentProvider cp = (ComponentProvider) sr.getBundle().getBundleContext().getService(sr);
+            providers.add(new ExtendedComponentProvider(cp, sr.getBundle().getSymbolicName()));
+        }
+
+        ExternalComponentRegistryImpl.getInstance().refreshComponents(providers);
+        MenuExtensionManager.getInstance().refreshExtendedMenus(providers);
+        PolicyManagerImpl.getInstance().refreshExtendedPolicies(providers);
+        ProviderDelegateServiceImpl.getInstance().refresh(providers);
+        StatusAreaWidgetRegistryImpl.getInstance().refresh(providers);
+        SearchServiceImpl.getInstance().refresh(providers);
+        componentProvidersChanged = true;
+        
+        if (t == null && pluginsLoaded) {
+            t = new Timer("Last Component Provider Loaded Checker", true);
+            t.scheduleAtFixedRate(new TimerTask() {
+             
+                 @Override
+                 public void run() {
+                     synchronized(LaunchMCTService.this) {
+                         if (componentProvidersChanged) {
+                             componentProvidersChanged = false;
+                             // wait until the next time to see if changes have stopped
+                         } else {
+                             requiresRefresh = true;
+                             t.cancel();
+                             t = null;
+                             SwingUtilities.invokeLater(new Runnable() {
+                                 @Override
+                                 public void run() {
+                                     initUserInterface();
+                                 }
+                             });
+                         }
+                     }
+                 }
+                 
+            }, 5000, 1000);
+        }
+        
+        if (requiresRefresh) {
+            refreshUI();
+        }
+    }
+    
+    private void refreshUI() {
+        GlobalContext.getGlobalContext().switchUser(GlobalContext.getGlobalContext().getUser(), new Runnable() {
+            public void run() {
+                PlatformImpl.getInstance().getWindowManager().refreshWindows();
+            }
+        });
+    }
+    
+    public synchronized void componentProviderAdded(ServiceReference cp) {
+        allProviders.add(cp);
+        notifyPlatformServicesChanged();
+    }
+    
+    public synchronized void componentProviderRemoved(ServiceReference cp) {
+        allProviders.remove(cp);
+        notifyPlatformServicesChanged();
+    }
+    
+    public synchronized void activate(BundleContext context) {
+        try {
+            loadUser();
+            pluginsLoaded = true;
+            startOptionalModules();
+        } catch (Exception e) {
+            logger.error("unable to start MCT, exception thrown", e);
+            System.exit(0);
+        }
+    }
+    
+    public void deactivate(BundleContext context) {
+        
+    }
+    
+    private synchronized void initUserInterface() {
+        // For now, make the platform provide the window decorations,
+        // specifically for Linux. Using false here will use the platform
+        // style, with solid background color and platform-standard window
+        // controls.
+        // This method has to be invoked before a JFrame is instantiated, and
+        // it stays in effect for all JFrames until the method is called again.
+        JFrame.setDefaultLookAndFeelDecorated(false);
+
+        // Initialize the Look and Feel Setting object to the value in
+        // mct.properties file, or else use the default LAF.
+        String lookAndFeelStr = MCTProperties.DEFAULT_MCT_PROPERTIES.getProperty("mct.look.and.feel");
+        LookAndFeelSettings.INSTANCE.setLAF(lookAndFeelStr);
+        if (! LookAndFeelSettings.INSTANCE.isInitialized()) {
+            logger.error("Could not initialize the Swing Look and Feel settings, MCT is closing.");
+            System.exit(1);
+        }
+
+        initUserEnvironment();
+        requiresRefresh = true;
+    }
+    
+    private void initUserEnvironment() {
+        new UserEnvironment();
+    }
+    
+    private void startOptionalModules() {
+        OSGIRuntime osgiRuntime = EquinoxOSGIRuntimeImpl.getOSGIRuntime();
+        osgiRuntime.startExternalBundles();
+    }
+    
+    private boolean loadUser() {
+        String whoami = GlobalContext.getGlobalContext().getIdManager().getCurrentUser();
+        User currentUser = PlatformAccess.getPlatform().getPersistenceProvider().getUser(whoami);
+        if (currentUser == null) {
+            final String ADD_USER_PROP = "automatically.add.user";
+            final String DEFAULT_GROUP_PROP = "default.user.group";
+            if (Boolean.parseBoolean(System.getProperty(ADD_USER_PROP, MCTProperties.DEFAULT_MCT_PROPERTIES.getProperty(ADD_USER_PROP, "false")))) {
+                String userId = whoami;
+                Platform platform = PlatformAccess.getPlatform();
+                
+                // determine if the platform has been initialized
+                if (platform.getBootstrapComponents().isEmpty()) {
+                    platform.getDefaultComponentProvider().createDefaultComponents();
+                    // invoke getting default components again to ensure the platform has been bootstrapped
+                    platform.getBootstrapComponents();
+                }
+                
+                AbstractComponent mySandbox = platform.getDefaultComponentProvider().createSandbox(userId);
+                AbstractComponent dropbox = platform.getDefaultComponentProvider().createDropbox(userId);
+                
+                String group = System.getProperty(DEFAULT_GROUP_PROP, MCTProperties.DEFAULT_MCT_PROPERTIES.getProperty(DEFAULT_GROUP_PROP, ""));
+                if (group.isEmpty()) {
+                    throw new MCTRuntimeException("Default group not specified, set the default group in mct.properties using the " + DEFAULT_GROUP_PROP + " property.");
+                }
+                platform.getPersistenceProvider().addNewUser(userId, group, mySandbox, dropbox);
+                currentUser = platform.getPersistenceProvider().getUser(whoami);
+            } else {
+                throw new MCTRuntimeException("MCT user '" + whoami
+                        + "' is not in the MCT database. You can load MCT user(s) using MCT's load user tool.");
+            }
+        }
+        GlobalContext.getGlobalContext().switchUser(currentUser, null);
+        return true;
+    }
+}
