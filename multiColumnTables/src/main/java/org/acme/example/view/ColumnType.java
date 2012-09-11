5048c35f111df6507ca95b4079ff2677748b6c1f
alpearson <ampearso@ucsc.edu>
2012-09-10 17:40:20 -0700

    half of the pastes syncing up code lost on monday

--- multiColumnTables/src/main/java/org/acme/example/view/ColumnType.java
+++ multiColumnTables/src/main/java/org/acme/example/view/ColumnType.java
@@ -1,5 +1,25 @@
 package org.acme.example.view;
 
 public enum ColumnType {
-	ID, VALUE, TITLE, ALARM_STATE, ERT
+	ID("ID"),
+	TITLE("Title"),
+	FSW_NAME("FSW Name"),
+	RAW("Raw"),
+	VALUE("Value"),
+	UNIT("Unit"),
+	ERT("ERT"),
+	SCLK("SCLK"),
+	SCET("SCET");
+	
+	private final String text;
+	
+	private ColumnType(final String text) {
+		this.text = text;
+	}
+	
+	@Override
+	public String toString() {
+		return text;
+	}
 }
+
