c808481c0a119e564a6b3eeb5c291830a50c2972
alpearson <ampearso@ucsc.edu>
2012-09-20 14:12:26 -0700

    Fixed bug where TIME column had inconsistent format

--- multiColumnTables/src/main/java/org/acme/example/view/TimeCellRender.java
+++ multiColumnTables/src/main/java/org/acme/example/view/TimeCellRender.java
@@ -32,10 +32,8 @@ import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
-
 @SuppressWarnings("serial")
 public class TimeCellRender extends DefaultTableCellRenderer {
-
 	private Format TIMEFormatter = new SimpleDateFormat("HH:mm:ss.S");
 
 	public TimeCellRender() {
@@ -45,16 +43,11 @@ public class TimeCellRender extends DefaultTableCellRenderer {
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column) {
-
 		JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
 		assert value instanceof FeedProvider;
 		FeedProvider feedProvider = (FeedProvider) value;
-		if (column == ColumnType.TIME.ordinal()) {
-			label.setText(TIMEFormatter.format(feedProvider.getTimeService().getCurrentTime()));
-		} else {
-			label.setText(Long.toString(feedProvider.getTimeService().getCurrentTime()));
-		}
+		label.setText(TIMEFormatter.format(feedProvider.getTimeService().getCurrentTime()));
 		return label;
 
 	}
