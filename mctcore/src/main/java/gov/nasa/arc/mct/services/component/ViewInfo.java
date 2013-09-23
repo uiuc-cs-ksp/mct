adc63ec9e390456a520b41dcb5c56241939ea687
Victor Woeltjen <victor.woeltjen@nasa.gov>
2013-09-23 16:17:13 -0700

    [Views] Disregard view type when testing info equality
    Certain set operations upon ViewInfo objects
    are contingent upon the treatment of ViewInfo
    objects describing the same class as equal,
    even if they apply to different view types.
    Failure to exhibit this sort of equality is
    the root cause of nasa/mct#195.
    
    This change removes consideration of view
    type and hashCode from equals (this was
    added erroneously in a previous commit
    triggering the regression.)

--- mctcore/src/main/java/gov/nasa/arc/mct/services/component/ViewInfo.java
+++ mctcore/src/main/java/gov/nasa/arc/mct/services/component/ViewInfo.java
@@ -211,7 +211,6 @@ public class ViewInfo extends TypeInfo<View> {
     @Override
     public int hashCode() {
         return getType().hashCode() ^
-               getViewType().hashCode() ^
                (preferredComponentType == null ? 0 : preferredComponentType.hashCode());
     }
     
@@ -219,7 +218,6 @@ public class ViewInfo extends TypeInfo<View> {
     public boolean equals(Object obj) {
         return obj instanceof ViewInfo && 
                ((ViewInfo)obj).getType().equals(getType()) && 
-               ((ViewInfo)obj).getViewType().equals(getViewType()) &&
                ((ViewInfo)obj).getPreferredComponentType() == preferredComponentType;
     }
     
