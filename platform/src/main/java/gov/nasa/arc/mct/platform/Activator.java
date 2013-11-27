6724f997158144ce478b2ebdd04eef45ccd92052
Victor Woeltjen <victor.woeltjen@nasa.gov>
2013-11-27 12:32:51 -0800

    [Platform] Adjust timing of persistence dialog
    Wait longer (ten seconds instead of five) before
    showing a warning that persistence is taking too
    long to initialize. (This warning message is
    associated with nasa/mct#225)
    
    This extra duration should help minimize
    instances where the user is shown the warning
    when a database is actually available.

--- platform/src/main/java/gov/nasa/arc/mct/platform/Activator.java
+++ platform/src/main/java/gov/nasa/arc/mct/platform/Activator.java
@@ -128,7 +128,7 @@ public class Activator implements BundleActivator {
                 // Otherwise, fall back to this bundle's version.
                 return WindowManagerImpl.getInstance();
             }
-        }, 5000, 55000); // Warn after five seconds, kill after one minute        
+        }, 10000, 50000); // Warn after ten seconds, kill after one minute        
 
         initServicesAndHandlers(context);
     }
