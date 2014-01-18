db09a4cb99f8f38093b6d796e2de450d146e4196
Daniel Pacak <pacak.daniel@gmail.com>
2014-01-18 22:38:17 +0100

    refactor startup scripts (use variables)

--- platform-assembly/src/main/resources/bin/mct.bat
+++ platform-assembly/src/main/resources/bin/mct.bat
@@ -1,2 +1,10 @@
-cd ..
-java -jar lib\startup-${project.version}.jar
+@echo off
+
+set _REALPATH=%~dp0
+set _MCT_BASE=%_REALPATH%..\
+set _MCT_LIB=%_MCT_BASE%lib\
+set _MCT_LAUNCHER_JAR=%_MCT_LIB%startup-${project.version}.jar
+set _JAVA_EXE=java
+
+cd %_MCT_BASE%
+%_JAVA_EXE% -jar "%_MCT_LAUNCHER_JAR%"
--- platform-assembly/src/main/resources/bin/mct.sh
+++ platform-assembly/src/main/resources/bin/mct.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd ..
-java -jar lib/startup-${project.version}.jar
+
+_REALPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+_MCT_BASE="$_REALPATH/.."
+_MCT_LIB="$_MCT_BASE/lib"
+_MCT_LAUNCHER_JAR="$_MCT_LIB/startup-${project.version}.jar"
+_JAVA_EXE=java
+
+cd $_MCT_BASE
+
+$_JAVA_EXE -jar $_MCT_LAUNCHER_JAR
