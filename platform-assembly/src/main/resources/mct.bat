ff06bf09842415614eb97503977875939efc3dca
Daniel Pacak <daniel.pacak@hrs.de>
2014-01-25 01:35:51 +0100

    use relative path so that Cygwin does not complain

--- platform-assembly/src/main/resources/mct.bat
+++ platform-assembly/src/main/resources/mct.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set _MCT_BASE=%~dp0
-set _MCT_LIB=%_MCT_BASE%lib\
-set _MCT_LAUNCHER_JAR=%_MCT_LIB%startup-${project.version}.jar
+set _MCT_LIB=lib
+set _MCT_LAUNCHER_JAR=%_MCT_LIB%\startup-${project.version}.jar
 set _JAVA_EXE=java
 
 cd %_MCT_BASE%
